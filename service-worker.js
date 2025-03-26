88cb4db86d2b7feeda94603e79cd71078375f89f
PSBahia <paulo.rpba@gmail.com>
2025-03-26 15:22:44 -0300

    alteracao no service worker

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,25 @@
-const cacheName = 'v1';
-const cacheAssets = [
-    './Home/index.html',
-    './Home/styles.css',
-    './Home/script.js',
-    './',
-    './manifest.json',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+const staticCacheName = 'coleta-simples-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json'
 ];
 
-// Call Install Event
-self.addEventListener('install', e => {
-    console.log('Service Worker: Installed');
-
-    e.waitUntil(
-        caches
-            .open(cacheName)
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName)
             .then(cache => {
-                console.log('Service Worker: Caching Files');
-                cache.addAll(cacheAssets);
+                return cache.addAll(assetsToCache);
             })
-            .then(() => self.skipWaiting())
     );
 });
 
-// Call Activate Event
-self.addEventListener('activate', e => {
-    console.log('Service Worker: Activated');
-    // Remove unwanted caches
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-});
-
-// Call Fetch Event
-self.addEventListener('fetch', e => {
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
