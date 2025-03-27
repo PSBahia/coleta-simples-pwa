8c214b2ed9980f6bcfa124c0e0e379d5f6f7eee0
PSBahia <paulo.rpba@gmail.com>
2025-03-26 22:54:00 -0300

    alteracao no service-worker(offline)

--- service-worker.js
+++ service-worker.js
@@ -7,16 +7,22 @@ self.addEventListener('install', (event) => {
         '/styles.css',
         '/app.js',
         '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        '/icons/icon-512x512.png',
+        '/manifest.json'
       ]);
+    }).catch((err) => {
+      console.error('Erro ao adicionar arquivos ao cache:', err);
     })
   );
+  console.log('Service Worker instalado');
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    }).catch((err) => {
+      console.error('Erro ao buscar recurso:', err);
     })
   );
 });
