2805ec85c4b9ea22184fe01f3b71bf6255bc84ad
PSBahia <paulo.rpba@gmail.com>
2025-03-28 00:25:16 -0300

    configuração do escopo service-worker no app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-    console.log('Service Worker registrado com sucesso:', registration);
-  }).catch((error) => {
-    console.error('Falha ao registrar o Service Worker:', error);
-  });
+  navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+      .then((registration) => {
+          console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch((error) => {
+          console.error('Falha ao registrar o Service Worker:', error);
+      });
 }
 
 let deferredPrompt;
